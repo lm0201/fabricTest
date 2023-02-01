83aa68f89911097a4bada6e39041ab14edd0d45b
lm0201 <61541683+lm0201@users.noreply.github.com>
2023-02-01 18:15:55 +0800

    feat: fabric 初体验

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { fabric } from "fabric";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const canvas = new fabric.Canvas("canvas");
+    const rect = new fabric.Rect({
+      top: 20,
+      left: 20,
+      width: 20,
+      height: 20,
+      fill: "green",
+    });
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    //canvas.add();
+    const Circle = new fabric.Circle({
+      radius: 20,
+      fill: "green",
+      left: 100,
+      top: 100,
+    });
+    canvas.add(rect, Circle);
+    rect.animate(
+      { top: 50, left: 400, angle: 45 },
+      { duration: 1000, onChange: canvas.renderAll.bind(canvas) }
+    );
+  }, []);
+  return <canvas id="canvas" width="1000px" height="1000px"></canvas>;
 }
 
-export default App
+export default App;
